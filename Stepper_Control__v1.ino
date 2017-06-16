c1e202de68ca5eed4694cb26ee5bc2c9036f6614
freddy887 <h.laeuferts@tu-bs.de>
2017-06-16 16:54:54 +0200

    Add files via upload
    Version 1. Stepper Control mit Initialisierung

---
+++ Stepper_Control__v1.ino
@@ -0,0 +1,34 @@
+#include <CheapStepper.h>
+
+CheapStepper stepper (8,9,10,11);
+
+boolean runter = true;
+int x; 
+int StepsFenster;
+void setup() {
+
+  stepper.setRpm (10); // Drehzahl hier einstellen (6 RPM -22 RPM) Drehzahl gegen Drehmoment
+  Serial.begin(9600);
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  for (x; digitalRead(2)==HIGH; x++)
+    {
+      runter = true;
+      stepper.step(runter);
+  }
+  StepsFenster=0;
+  for (StepsFenster; digitalRead(3)==HIGH;StepsFenster++) 
+    {
+      runter = false;
+      stepper.step(runter);
+  }
+  Serial.print("Steps between open and closed window: ");
+  Serial.print(StepsFenster);
+}
+
+void position(boolean runter, int 
+
+void loop() {
+  
+}
+
