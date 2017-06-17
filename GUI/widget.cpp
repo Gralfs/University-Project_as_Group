7fdd5bd0f04a7f053eba9d2f88c26cb00e1dd218
PatrickStad <p.stadler@tu-bs.de>
2017-06-17 16:24:58 +0200

    Gui added

---
+++ GUI/widget.cpp
@@ -0,0 +1,55 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    //setStyleSheet("QPushButton#pushButton_2 { background-color: red; }\n"
+                 // "QPushButton#pushButton_5 { background-color: grey; }\n");
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+/*
+void Widget::on_pushButton_3_clicked()   //Fully open blinds
+{
+
+}
+
+void Widget::on_pushButton_4_clicked()   //Fully close blinds
+{
+
+}
+
+void Widget::on_pushButton_2_clicked()   //Emergency stop
+{
+
+}
+
+void Widget::on_pushButton_5_clicked()   //Shutdown the whole programm (maybe wanted for holidays)
+{
+
+}*/
+
+void Widget::on_checkBox_stateChanged(int arg1)     //Set button colors when manuel mode is activated
+{
+
+    if (arg1 == 2){
+        setStyleSheet("QPushButton#pushButton_2 { background-color: red; }\n"
+                      "QPushButton#pushButton_5 { background-color: grey; }\n"
+                      "QPushButton#pushButton_3 { background-color: yellow; }\n"
+                      "QPushButton#pushButton_4 { background-color: black; }\n"
+                      "QPushButton#pushButton_4 { color: white; }\n");
+    }
+
+    if (arg1 == 0){
+        setStyleSheet("");
+    }
+
+}
+
+//Slidersetup still missing
---
+++ GUI/widget.h
@@ -0,0 +1,38 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = 0);
+    ~Widget();
+
+/*private slots:
+    void on_verticalSlider_rangeChanged(int min, int max);
+
+    void on_pushButton_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_5_clicked();*/
+
+private slots:
+    void on_checkBox_stateChanged(int arg1);
+
+private:
+    Ui::Widget *ui;
+};
+
+#endif // WIDGET_H
