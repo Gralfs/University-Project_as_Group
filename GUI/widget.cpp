df1431cb744d235eade6ce53d9c2d46f6eaebddc
PatrickStad <p.stadler@tu-bs.de>
2017-06-23 19:53:28 +0200

    Auf/Zu are usable and when in auto mode buttons are disabled

--- GUI/widget.cpp
+++ GUI/widget.cpp
@@ -6,8 +6,6 @@ Widget::Widget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Widget)
 {
-    //setStyleSheet("QPushButton#pushButton_2 { background-color: red; }\n"
-                 // "QPushButton#pushButton_5 { background-color: grey; }\n");
     ui->setupUi(this);
 }
 
@@ -16,24 +14,11 @@ Widget::~Widget()
     delete ui;
 }
 /*
-void Widget::on_pushButton_3_clicked()   //Fully open blinds
-{
-
-}
-
-void Widget::on_pushButton_4_clicked()   //Fully close blinds
-{
-
-}
-
 void Widget::on_pushButton_2_clicked()   //Emergency stop
 {
-
 }
-
 void Widget::on_pushButton_5_clicked()   //Shutdown the whole programm (maybe wanted for holidays)
 {
-
 }*/
 
 void Widget::on_checkBox_stateChanged(int arg1)     //Set button colors when manuel mode is activated
@@ -45,23 +30,39 @@ void Widget::on_checkBox_stateChanged(int arg1)     //Set button colors when man
                       "QPushButton#pushButton_3 { background-color: yellow; }\n"
                       "QPushButton#pushButton_4 { background-color: black; }\n"
                       "QPushButton#pushButton_4 { color: white; }\n");
+        ui->pushButton_2->setEnabled(true);
+        ui->pushButton_3->setEnabled(true);
+        ui->pushButton_4->setEnabled(true);
+        ui->pushButton_5->setEnabled(true);
+        ui->verticalSlider->setEnabled(true);
     }
 
     if (arg1 == 0){
         setStyleSheet("");
+        ui->pushButton_2->setEnabled(false);
+        ui->pushButton_3->setEnabled(false);
+        ui->pushButton_4->setEnabled(false);
+        ui->pushButton_5->setEnabled(false);
+        ui->verticalSlider->setEnabled(false);
     }
 
 }
-
-//Slidersetup still missing
 
 void Widget::on_verticalSlider_sliderMoved(int state)        //state of slider
 {
     int posi = state +1;
     QString pos = QString::number(posi);
     ui->label_2->setText(pos);
-/*    int position = 75;
-    QString posit = QString::number(position);
-    ui->label->setText(posit);
-    ui->progressBar->setValue(position);*/
+}
+
+void Widget::on_pushButton_3_clicked()   //Fully open blinds
+{
+    ui->verticalSlider->setValue(0);
+    ui->label_2->setText("1");
+}
+
+void Widget::on_pushButton_4_clicked()   //Fully close blinds
+{
+    ui->verticalSlider->setValue(99);
+    ui->label_2->setText("100");
 }
--- GUI/widget.h
+++ GUI/widget.h
@@ -1,3 +1,4 @@
+
 #ifndef WIDGET_H
 #define WIDGET_H
 
@@ -15,15 +16,10 @@ public:
     explicit Widget(QWidget *parent = 0);
     ~Widget();
 
-/*private slots:
-    void on_pushButton_clicked();
-
+private slots:
     void on_pushButton_3_clicked();
-
     void on_pushButton_4_clicked();
-
-    void on_pushButton_2_clicked();
-
+/*    void on_pushButton_2_clicked();
     void on_pushButton_5_clicked();*/
 
 private slots:
