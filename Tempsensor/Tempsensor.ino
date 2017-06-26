223a552ce244086aecd939b97732508161a22665
Handlanger <jn123@gmx.net>
2017-06-26 23:55:22 +0200

    Add files via upload
    Test

---
+++ Tempsensor/Tempsensor.ino
@@ -0,0 +1,13 @@
+int TMP36 = A0; //Pinbelegung Sensor, wird noch angepast
+int temp_sensor;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
