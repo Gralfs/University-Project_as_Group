917a8cfa6368be6d69d09d09e589d6b1eacd12ee
Handlanger <jn123@gmx.net>
2017-06-27 00:05:39 +0200

    Update Tempsensor.ino

--- Tempsensor/Tempsensor.ino
+++ Tempsensor/Tempsensor.ino
@@ -6,10 +6,11 @@ int t = 1000; //Zeitspanne zwischen Temperaturabfragen;
 
 void setup() {
   // put your setup code here, to run once:
-
+Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+temp_sensor=analogRead(TMP36) //Auslesen des temp_sensor an Pin
+  
 }
