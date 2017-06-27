ed8d9d885364914b3cf767565c698d9f45d1889c
Handlanger <jn123@gmx.net>
2017-06-27 21:53:21 +0200

    Update Tempsensor.ino

--- Tempsensor/Tempsensor.ino
+++ Tempsensor/Tempsensor.ino
@@ -1,11 +1,15 @@
+
+#include <EEPROM.h>
+
+
 int TMP36 = A0; //Pinbelegung Sensor, wird noch angepast
 int temp_sensor; //Intwerte zwischen 0 bis 410
 int temp_celsius = 0; //Umgerechnete Temperatur
 int t = 1000; //Zeitspanne zwischen Temperaturabfragen;
 int i;
-int Werte [5];
-
-
+int Werte [60];
+int mittelwert;
+int j;
 
 
 void setup() {
@@ -16,7 +20,7 @@ Serial.begin(9600); //Übertragung Serielle Schnittstelle
 void loop() {
 // put your main code here, to run repeatedly:
 
-
+for (j=1; j<2; j++){
 for (i=0; i<60; i++){
    
    temp_sensor = analogRead(TMP36); //Auslesen des temp_sensor an Pin
@@ -27,10 +31,17 @@ for (i=0; i<60; i++){
 
    
    Werte[i] = temp_celsius;
-   
+
+
+  
    delay(t);
   }
 
+
+
+  EEPROM.write(j, mittelwert); 
+   
+}
 
  
 }
