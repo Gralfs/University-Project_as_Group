2615ae98ccb66f807bb9bb36e7fdc5c4410d7026
Michael Schlangen <m.schlangen@tu-braunschweig.de>
2017-07-09 14:07:23 +0200

    Hilfsfunktionen für bessere Lesbarkeit

--- Arduino_Program_v1.1/Arduino_Program_v1.1.ino
+++ Arduino_Program_v1.1/Arduino_Program_v1.1.ino
@@ -55,64 +55,91 @@ boolean runter = true;         //Boolean variable das der Steppermotor weiß ob 
 void rollo (int location);     //Funktiondefinition für das ändern der Rollo position
 Arduino Rollo;                  //Rollo als Arduino definiert 
 
-int initialise () {                 //Hier wird das Rollo intialisiert 
-  while (digitalRead(4)!=LOW)       //Das Rollo fährt einmal bis ganz nach Unten bis er zum unterem Kontakt kommt
-    {
-      runter = true;                //wenn runter=true fährt das Rollo runter
-      stepper.step(runter);           //Steppermotor befehl das er unendlich lange runter fährt
-      //Serial.print("While loop");     //Ausgabe while loop für debugging. Das man weiß wo sich der Arduino gerade befindet
+
+bool isAtTop() {
+  return digitalRead(3) == LOW;
+}
+
+bool isAtBottom() {
+  return digitalRead(4) == LOW;
+}
+
+void setupStepper() {
+  stepper.setRpm (10); // Drehzahl hier einstellen (6 RPM -22 RPM) Drehzahl gegen Drehmoment
+}
+
+void driveUp() {
+  stepper.step(false);
+}
+
+void driveDown() {
+  stepper.step(true);
+}
+
+int setupRollo() {
+  //Das Rollo fährt einmal bis ganz nach Unten bis er zum unterem Kontakt kommt
+  while (!isAtBottom()) {
+    driveDown();
   }
-  int StepsFenster=0;                   //definition das die steps zwischen ganz unten und ganz oben gespeichert werden kann
-  for (StepsFenster; digitalRead(3)==HIGH;StepsFenster++)    //Jetzt fährt das Rollo ganz nach Oben und zählt dabei die notwendigen Schritte
-    {
-      runter = false;    //jetztz ist runter=false also fährt das Rollo nach oben 
-      stepper.step(runter);   //Das Rollo fährt so unendlich nach Oben
-      //Serial.print(StepsFenster);    //Serial print für das Debugging
-      //Serial.print("\n");
+
+  //Jetzt fährt das Rollo ganz nach Oben und zählt dabei die notwendigen Schritte
+  int StepsFenster = 0;
+  for (StepsFenster; !isAtTop(); StepsFenster++) {
+    driveUp();
   }
-  //Serial.print("TOTAL:");  //Weitere serial prints für das Debugging
-  //Serial.print(StepsFenster);
-  //Serial.print("\n");
+  
   Rollo.SetPositionInSteps (0); //Jetzt wo wir wissen das das Rollo ganz oben ist können wir die Position definieren
   Rollo.SetStepsWindow (StepsFenster); //die nötigen steps um ganz nach unten zufahren wird in der Klasse gespeichert
 }
 
+// Erstelle den String, der den Licht- und Temperaturwert ausgibt.
+String getSerialOutput(int licht, int temp) {
+  return String(licht) + "," + String(temp) + "|";
+}
+
 void setup() {       //Setup beim starten vom Arduino
-  Arduino Rollo;      //Klassen definition 
-  stepper.setRpm (10); // Drehzahl hier einstellen (6 RPM -22 RPM) Drehzahl gegen Drehmoment
-  Serial.begin(9600);   //Serial baud rate wird hier bestimmt 
+  //Serial baud rate wird hier bestimmt 
+  Serial.begin(9600);
+
   pinMode(4, INPUT_PULLUP);   //Die beiden kontakte für Rollo ganz offen und ganz zu, werden hier definiert 
   pinMode(3, INPUT_PULLUP);
-  initialise (); //Hier wird das Rollo intialisiert
-  //Serial.print("Setup"); //Serial ausgabe für Debugging, das man weiß wo sich das Programm gerade befindet
+  
+  setupStepper();
+  setupRollo();
 }
 
 void loop() {                  //Loop schleife, hier wird das Rollo gesteuert und die Sensorwerte ausgegeben
-    if (Serial.available() > 0){   //Wenn ein Serial zugang da ist wird integer location desen wert gleich gesätzt
-    int location = Serial.read();
-    rollo(location);}//Die funktion rollo kriegt dann den Wert vom Serial
-    int temp_sensor = analogRead(A8); //Auslesen des temp_sensor an Pin
-    int temp_celsius = map(temp_sensor, 0, 410, -50, 150); //Umwandeln in Grad Celsius
-    int foto_sensor =analogRead(A1);
-    int foto_base100= map(foto_sensor, 0, 410, -50, 150);
-    Serial.print(101);
-    Serial.print(",");
-    Serial.print(temp_celsius);
-    Serial.print(",");
-    Serial.print(102);
-    Serial.print(",");
-    Serial.print(foto_base100);
-    Serial.print(",");
-    Serial.print(103);
-    Serial.print(",");
-    Serial.print(Rollo.getPosition100());
-    Serial.print(",");
-    Serial.print("STOP");
-    Serial.print(Rollo.getStepsFenster());
-    Serial.print(",");
-    Serial.print("\n");
-    delay (1000);
   
+  if (Serial.available() > 0) {   //Wenn ein Serial zugang da ist wird integer location desen wert gleich gesätzt
+    int location = Serial.read();
+    rollo(location);
+  }//Die funktion rollo kriegt dann den Wert vom Serial
+
+  int temp_sensor = analogRead(A8); //Auslesen des temp_sensor an Pin
+  int temp_celsius = map(temp_sensor, 0, 410, -50, 150); //Umwandeln in Grad Celsius
+  int foto_sensor =analogRead(A1);
+  int foto_base100= map(foto_sensor, 0, 410, -50, 150);
+
+  Serial.write(getSerialOutput(foto_base100, temp_celsius).c_str());
+
+  
+  /*Serial.print(101);
+  Serial.print(",");
+  Serial.print(temp_celsius);
+  Serial.print(",");
+  Serial.print(102);
+  Serial.print(",");
+  Serial.print(foto_base100);
+  Serial.print(",");
+  Serial.print(103);
+  Serial.print(",");
+  Serial.print(Rollo.getPosition100());
+  Serial.print(",");
+  Serial.print("STOP");
+  Serial.print(Rollo.getStepsFenster());
+  Serial.print(",");
+  Serial.print("\n");*/
+  delay (1000);
 }
 
 void rollo (int location){
