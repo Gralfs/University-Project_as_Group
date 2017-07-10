45824d26d11f8cb9aaa893997c96f2012c0c9fa3
Handlanger <jn123@gmx.net>
2017-07-10 14:37:40 +0200

    Aufgeräumt und vervollständigt

---
+++ sensor_komplett/sensor_komplett_test/testDefinitionen.h
@@ -0,0 +1,46 @@
+#ifndef TESTDEFINITIONEN_H
+#define TESTDEFINITIONEN_H
+
+#ifdef TEST
+
+#include "test.h"
+
+extern boolean sensorwerteausgabe;
+
+void testAnmeldung(){ // Name frei waehlbar
+  bool testResult = false;
+
+  // Eigene Testimplementierung...
+
+  if(sensorwerteausgabe==true)
+{testResult = true;}
+  // testResult kann bei einem erfolgreichen Test 
+  // auf true gesetzt werden
+  
+  APITest::printTestResult(testResult, "Anmeldung", "Paul Frost",
+  "Anmeldung der Studierenden Prototyp A02", "testDefinitionen.h");
+}
+
+
+void runTests(){
+  APITest::printTestStartHeader(); // Nicht modifizieren
+  
+  
+
+
+
+  // Hier sollen die eigenen Tests hinzugefuegt werden
+  testAnmeldung();
+
+  APITest::printTestEndFooter(); // Nicht modifizieren
+}
+#endif //TEST
+
+#ifdef TEST  // Nicht modifizieren
+  #define RUNTEST runTests();
+#else
+  #define RUNTEST
+#endif
+
+
+#endif //TESTDEFINITIONEN_H
